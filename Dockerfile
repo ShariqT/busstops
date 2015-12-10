ba0fc54887f9d6eb6db1be894d1b9cf5f5f6e743
Shariq Torres <shariq.torres@gmail.com>
2015-12-10 03:27:28 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM ubuntu
+
+RUN apt-get update
+
+RUN apt-get install -y build-essential
+
+RUN apt-get install -y python2.7 python2.7-dev
+
+RUN apt-get install -y curl
+
+RUN apt-get install -y wget
+
+RUN apt-get install -y git
+
+RUN wget https://bootstrap.pypa.io/get-pip.py
+
+RUN python2.7 get-pip.py
+
+EXPOSE 10817
+
+RUN curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
+
+RUN apt-get install -y nodejs
+
+RUN npm install -g bower
+
+RUN npm install
+
+RUN bower install -s --allow-root
+
+VOLUME /var/log/busstops
+
+CMD nodejs app.js
---
+++ maputils/index.js
@@ -0,0 +1,62 @@
+var mquery = require('mquery');
+var mongo = require('mongodb').MongoClient;
+
+
+
+exports.normalizeLatLng = function(coordinates){
+	//this is lng, lat order
+	var lng = coordinates[0],
+	 	lat = coordinates[1];
+
+	return [lat,lng];
+
+};
+
+exports.findNearestBusStop = function(geoInfo, clientHours, school, cb){
+	mongo.connect('mongodb://bususer:bususer@ds053658.mongolab.com:53658/busstops', function(err, db){
+		if(err){
+		 cb(err, []);
+		}else{
+			console.log(school);
+			var busstops = db.collection('routes');
+			var letter;
+			(clientHours < 8) ? letter = "P" : letter = "D";
+			console.log(letter);
+
+			mquery(busstops).where('loc').near({ center: [geoInfo.results[0].geometry.location.lng, geoInfo.results[0].geometry.location.lat], 
+				spherical: true, maxDistance: 0.3 })
+					.where('school').regex('.*' + school +'.*')
+					.where('route_num').regex(letter)
+					.where('type').equals('STOP')
+					.exec(function(err, docs){
+						var error = null;
+						if(err) error = err;
+						console.log('found bus stop!');
+						db.close();
+						cb(error, docs);
+			});
+		}		
+	});
+}
+
+
+exports.getRoute = function(routeLabel, cb){
+	mongo.connect('mongodb://bususer:bususer@ds053658.mongolab.com:53658/busstops', function(err, db){
+		if(err) throw err;
+		console.log(routeLabel);
+		var busstops = db.collection('routes');
+
+		mquery(busstops)
+			.where('route_num')
+			.equals(routeLabel)
+			.sort({time:'asc'})
+			.exec(function(err, docs){
+				var error = null;
+				if(err) error = err;
+				console.log('found routes!');
+				db.close();
+				cb(error, docs);
+			});
+	
+	});	
+}
